c62a0c62b257d5c4ffa3e2e7249497510f9a5ab3
Lesion45 <EgorKulagin2005@yandex.ru>
2024-02-24 16:03:01 +0300

    init server, add logger

---
+++ cmd/app/main.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"github.com/go-chi/chi/v5"
+	"github.com/lesion45/pinterest-clone/internal/config"
+	"log/slog"
+	"net/http"
+	"os"
+)
+
+func main() {
+	cfg := config.MustLoad()
+
+	log := setupLogger(cfg.Env)
+	log = log.With(slog.String("env", cfg.Env))
+
+	log.Info("initializing server", slog.String("address", cfg.Server.Address))
+	log.Debug("logger debug mode enabled")
+
+	router := chi.NewRouter()
+
+	server := &http.Server{
+		Addr:         cfg.Server.Address,
+		Handler:      router,
+		ReadTimeout:  cfg.Server.TimeOut,
+		WriteTimeout: cfg.Server.TimeOut,
+		IdleTimeout:  cfg.Server.IdleTimeOut,
+	}
+
+	if err := server.ListenAndServe(); err != nil {
+		log.Error("failed to start server")
+	}
+	log.Error("server stopped")
+}
+
+func setupLogger(env string) *slog.Logger {
+	var log *slog.Logger
+
+	switch env {
+	case "local":
+		log = slog.New(slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}))
+	case "dev":
+		log = slog.New(slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}))
+	case "prod":
+		log = slog.New(slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelInfo}))
+	}
+
+	return log
+}
---
+++ internal/config/config.go
@@ -0,0 +1,44 @@
+package config
+
+import (
+	"log"
+	"os"
+	"time"
+
+	"github.com/ilyakaznacheev/cleanenv"
+)
+
+type Config struct {
+	Env string `yaml:"env" env-default:"local"`
+	// StoragePath string     `yaml:"storage-path" env-required:"true"`
+	Server HTTPServer `yaml:"http-serever"`
+}
+
+type HTTPServer struct {
+	Address     string        `yaml:"address" env-default:"localhost:5000"`
+	TimeOut     time.Duration `yaml:"timeout" env-default:"4s"`
+	IdleTimeOut time.Duration `yaml:"idle-timeout" env-default:"60s"`
+}
+
+func MustLoad() *Config {
+	os.Setenv("CONFIG-PATH", "C:/Users/maus1/GolandProjects/pinterest-clone/config/local.yml")
+
+	configPath := os.Getenv("CONFIG-PATH")
+
+	if configPath == "" {
+		log.Fatal("Config path is not set")
+	}
+
+	// check if file exists
+	if _, err := os.Stat(configPath); os.IsNotExist(err) {
+		log.Fatalf("Config file %s does not exist", configPath)
+	}
+
+	var cfg Config
+
+	if err := cleanenv.ReadConfig(configPath, &cfg); err != nil {
+		log.Fatalf("error reading config: %s", err)
+	}
+
+	return &cfg
+}
