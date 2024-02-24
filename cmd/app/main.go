c03e4c594dba717b6f7d88a65184d8adef57fdef
Lesion45 <EgorKulagin2005@yandex.ru>
2024-02-24 16:59:21 +0300

    migrate from chi to gorilla/mux

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -1,7 +1,7 @@
 package main
 
 import (
-	"github.com/go-chi/chi/v5"
+	"github.com/gorilla/mux"
 	"github.com/lesion45/pinterest-clone/internal/config"
 	"log/slog"
 	"net/http"
@@ -17,7 +17,7 @@ func main() {
 	log.Info("initializing server", slog.String("address", cfg.Server.Address))
 	log.Debug("logger debug mode enabled")
 
-	router := chi.NewRouter()
+	router := mux.NewRouter()
 
 	server := &http.Server{
 		Addr:         cfg.Server.Address,
