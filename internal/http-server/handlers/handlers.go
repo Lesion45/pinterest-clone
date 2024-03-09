b1372143167e8ce09afb2fa0a09b87dd8182c685
Lesion45 <EgorKulagin2005@yandex.ru>
2024-03-09 11:25:57 +0300

    handlers init

---
+++ internal/http-server/handlers/handlers.go
@@ -0,0 +1,34 @@
+package handlers
+
+import (
+	"github.com/gin-gonic/gin"
+)
+
+// TODO: HOME PAGE
+func HomePage(ctx *gin.Context) {
+	return
+}
+
+func GetAllPins(ctx *gin.Context) {
+	return
+}
+
+// TODO: LOG IN
+func Login(ctx *gin.Context) {
+	return
+}
+
+// TODO: REGISTER
+func Register(ctx *gin.Context) {
+	return
+}
+
+// TODO: CREATE PIN
+func CreatePin(ctx *gin.Context) {
+	return
+}
+
+// TODO: GET PIN
+func GetPin(id int, ctx *gin.Context) {
+	return
+}
